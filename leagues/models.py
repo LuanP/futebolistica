c63522f2ecb163f4b22e02e444d360ba930e4a3d
Luan <luanpab@gmail.com>
2013-09-16 21:44:44 -0300

    Adding leagues app

---
+++ leagues/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class League(models.Model):
+    name = models.CharField(max_length=200)
+    period_start = models.DateField()
+    period_end = models.DateField()
+
+
+class Round(models.Model):
+    games = models.ManyToManyField('games.Game')
+    league = models.ForeignKey('League')
+
+    class Meta:
+        unique_together = ('games', 'league')
