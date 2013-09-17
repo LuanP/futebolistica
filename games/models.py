a791ccb34b3874d40d48d45945fb7feb9f5d98fb
Luan <luanpab@gmail.com>
2013-09-16 21:44:31 -0300

    Adding games app

---
+++ games/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Stadium(models.model):
+    name = models.CharField(max_length=200)
+
+
+class Judge(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class GameTeam(models.Model):
+    game = models.ForeignKey('Game')
+    team = models.ForeignKey('teams.Team')
+    score = models.SmallIntegerField()
+
+
+class Game(models.Model):
+    stadium = models.ForeignKey('Stadium')
+    judge = models.ForeignKey('Judge')
+    date = models.DateField()
+    team_1 = models.ForeignKey('teams.Team')
+    team_2 = models.ForeignKey('teams.Team')
