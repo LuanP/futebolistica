af53bb7d309b5b9f8526112382bc1d89a2f57eb5
Luan <luanpab@gmail.com>
2013-10-30 21:21:04 -0200

    Fixing team template

--- teams/views.py
+++ teams/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 
+from leagues.models import League
 from .models import Team
 
 
@@ -9,3 +10,13 @@ class TeamListView(ListView):
 
 class TeamDetailView(DetailView):
     model = Team
+
+    def get_context_data(self, **kwargs):
+        context = super(TeamDetailView, self).get_context_data(**kwargs)
+        context['players_by_league'] = []
+        for league in League.objects.all():
+            teamplayers = league.teamplayer_set.filter(
+                team__slug=self.kwargs.get('slug')
+            )
+            context['players_by_league'].append([league.name, teamplayers])
+        return context
