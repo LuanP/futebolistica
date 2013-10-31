0dd85e1438b9fff1505d51d1e6ae3459917172e4
Luan <luanpab@gmail.com>
2013-10-31 09:27:22 -0200

    Removing duplicated static url

--- futebolistica/heroku_settings.py
+++ futebolistica/heroku_settings.py
@@ -15,8 +15,6 @@ MANAGERS = ADMINS
 
 CWD = os.path.dirname(os.path.abspath((__file__))
 
-STATIC_URL = 'http://futebolistica.herokuapp.com/static/'
-
 ALLOWED_HOSTS = ['*', ]
 
 STATIC_ROOT = 'staticfiles'
