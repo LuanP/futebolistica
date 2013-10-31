34d35b05dbf891f151c959f7cd6dccb34f86ac06
Luan <luanpab@gmail.com>
2013-10-31 09:21:50 -0200

    Adding heroku wsgi to serve staticfiles

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:$PORT futebolistica.wsgi
+web: gunicorn -b 0.0.0.0:$PORT futebolistica.heroku_wsgi
--- futebolistica/heroku_settings.py
+++ futebolistica/heroku_settings.py
@@ -13,13 +13,18 @@ ADMINS = ADMINS = (
 
 MANAGERS = ADMINS
 
-CWD = os.path.abspath(os.path.dirname(__file__))
+CWD = os.path.dirname(os.path.abspath((__file__))
 
 STATIC_URL = 'http://futebolistica.herokuapp.com/static/'
 
 ALLOWED_HOSTS = ['*', ]
 
-STATIC_ROOT = os.path.join(CWD, 'static')
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(CWD, 'static'),
+)
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
