b7e4d292977cdda213b422bf8984eba53448f9e9
Luan <luanpab@gmail.com>
2013-10-30 23:26:05 -0200

    Minor fix on slicing teams

--- futebolistica/context_processors.py
+++ futebolistica/context_processors.py
@@ -3,7 +3,7 @@ from leagues.models import League
 
 
 def get_teams(request):
-    return dict(teams=Team.objects.all()[5:])
+    return dict(teams=Team.objects.all()[:5])
 
 def get_leagues(request):
     return dict(leagues=League.objects.all())
